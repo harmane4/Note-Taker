8af567081eebb4e897ad836ddd6e049d2814fbe5
Elise Harman <elise_harman@hotmail.com>
2021-05-16 14:13:33 +0800

    Added routes and API

--- server.js
+++ server.js
@@ -8,6 +8,19 @@ const path = require("path");
 const PORT = 1000; 
 const app = express();
 
+// Serving Static Files 
+app.use(express.static("public"));
+
+// Routes 
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "/public/index.html")));
+
+app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "/public/notes.html")));
+
+// Displays all notes 
+app.get("/api/notes", (req, res) => res.json(notes));
+
+
+
 // Starts the server to begin listening 
 
 app.listen(PORT, () =>
