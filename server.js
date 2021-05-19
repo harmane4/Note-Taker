6df43c252b87560ab88c312141710b20abe03f78
Elise Harman <elise_harman@hotmail.com>
2021-05-19 19:27:24 +0800

    Removed console logs

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const fs = require("fs");
 const express = require("express");
 const http = require("http");
 const path = require("path");
+const { response } = require("express");
 
 // Sets up Express App
 const port = process.env.PORT || 3000;
@@ -14,32 +15,30 @@ app.use(express.static("public"));
 app.use(express.json());
 
 // Routes
-app.get("/", (req, res) =>
-  res.sendFile(path.join(__dirname, "/public/index.html"))
+app.get("/", (request, response) =>
+  response.sendFile(path.join(__dirname, "/public/index.html"))
 );
 
-app.get("/notes", (req, res) =>
+app.get("/notes", (request, res) =>
   res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
 
 // Displays all notes
-app.get("/api/notes", (req, res) => {
+app.get("/api/notes", (request, response) => {
   // Read db.json file
   fs.readFile("./db/db.json", "utf8", (err, jsonString) => {
     if (err) {
       console.log("File read filed:", err);
       return;
     }
-    console.log("File data", jsonString);
-    res.json(JSON.parse(jsonString)); // JSON to array
+    response.json(JSON.parse(jsonString)); // JSON to array
   });
 });
 
 // POST
-app.post("/api/notes", (req, res) => {
-  const newNote = req.body;
+app.post("/api/notes", (request, response) => {
+  const newNote = request.body;
   newNote.id = Date.now()
-  console.log("newnote", newNote);
   // POST
   fs.readFile("./db/db.json", "utf8", (err, jsonString) => {
     if (err) {
@@ -48,16 +47,32 @@ app.post("/api/notes", (req, res) => {
     }
     console.log("File data", jsonString);
     // JSON to array
-    var notes = JSON.parse(jsonString); 
+    let notes = JSON.parse(jsonString); 
 
     notes.push(newNote);
     // ARRAY back to string
     fs.writeFile("./db/db.json", JSON.stringify(notes), (err) => {
       if (err) throw err;
-      return res.end();
+      return response.end();
     });
   });
 });
+
+// DELETE 
+// app.delete("/api/notes/:id" , (request, response) => {
+//   fs.readFile("./db/db.json", "utf8", (err, jsonString) => {
+//     if (err) {
+//       console.log("File read filed:", err);
+//       return;
+//     }
+//     let newNote = JSON.parse(jsonString);
+//     // delete note here
+//     fs.writeFile("./db/db.json", JSON.stringify(newNote), (err) => {
+//       if (err) throw err;
+//     });
+//     response.send("DELETE Request Called")
+// });
+// });
 
 // Starts the server to begin listening
 app.listen(port, () => console.log(`App listening on PORT ${port}`));
